75b8f8574adbc841e28b123889cb05c1e9f4ec6b
lesquel <lesquel1319@gmail.com>
2025-05-29 15:45:21 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tienda_angular';
